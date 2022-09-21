ca0289a48d17761be5309c5c9f1e6edc3020c97c
Vlad <103254700+zVzLzAzDz@users.noreply.github.com>
2022-09-21 20:19:24 +0300

    Initial commit

---
+++ src/PositiveNumber.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class PositiveNumber {
+
+    public static int inputPositiveNumber() {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        while (true) {
+            System.out.print("Input positive number: ");
+            number = scanner.nextInt();
+            if (number > 0) {
+                break;
+            }
+
+        }
+        return number;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println(inputPositiveNumber());
+    }
+}
+
+
